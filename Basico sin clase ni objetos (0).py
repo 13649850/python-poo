b7b09445fd8ad0fec2f62e26f38f87260f4accae
13649850 <francisperezmoreno2004@gmail.com>
2024-01-25 19:56:44 -0900

    Se agrega el codigo

---
+++ Basico sin clase ni objetos (0).py
@@ -0,0 +1,19 @@
+#ejemplo basico sin utilizar clase, objetos, contructores...
+
+celular1_marca = "samsung" 
+celular2_marca = "apple" 
+celular3_marca = "huawei" 
+
+celular1_modelo = "S23" 
+celular2_modelo = "iPhone 15 pro" 
+celular3_modelo = "P20 pro" 
+
+celular1_camaraF = "48MP" 
+celular2_camaraF = "48MP" 
+celular3_camaraF = "12MP" 
+
+celular1_camaraT = "24MP" 
+celular2_camaraT = "24MP" 
+celular3_camaraT = "8MP"
+
+print(celular1_camaraF)
